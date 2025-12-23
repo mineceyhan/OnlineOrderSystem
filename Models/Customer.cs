4bd77fc6147de49ae382f49fa988856d8c553afd
mine.ceyhan <mine.ceyhan@crssoft.com>
2025-12-23 16:25:49 +0300

    commit 2

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,11 +7,12 @@ public class Customer
     public int Id { get; }
     public string Email { get; }
     public int Phone { get; }
-
-    public Customer(int id, string email,int Phone )
+    public string Address { get; }
+    public Customer(int id, string email,int Phone , string address )
     {
         Id = id;
         Email = email;
+        Address = address;
         Logger.Log($"Customer registered: {Email}");
     }
 }
