83af8a68ab016d0674aa68c327b679d6a6c988a3
mine.ceyhan <mine.ceyhan@crssoft.com>
2025-12-24 09:35:21 +0300

    commit 5

--- Services/Test1.cs
+++ Services/Test1.cs
@@ -1,14 +1,18 @@
+using System;
+
+namespace Services
+{
 public class Test1
 
 {
     public string M1()
 
     {
-            string content = "<html><head></head><body><h1>Test</h1></body></html>";
+            string content = "<html><head></head><body><h1>NOVA-commit-5</h1></body></html>";
             return content;
 
     }
 
 }
-
+}
  
